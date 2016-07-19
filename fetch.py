bea55528a31a8c4eb05b68b9dda6da3b20138921
Paul Dodd <pdodd@umich.edu>
2016-07-19 11:12:28 -0400

    initial commit

---
+++ fetch.py
@@ -0,0 +1,32 @@
+import urllib.request
+import html5lib as html
+import bs4 as bs
+# soup = BeautifulSoup(html_doc, 'html.parser')
+
+def _fetch(url):
+    try:
+        data = urllib.request.urlopen(url)
+        return data.read();
+    except e:
+        raise(e)
+    return data.read();
+
+def _parse(htmldoc):
+    soup = bs.BeautifulSoup(htmldoc, 'html.parser')
+    divs = soup.find_all('div');
+    for div in divs:
+        if div.get_text() == "Core Classes":
+            print(div.get_text())
+            links = div.find_all('a'); 
+            for link in links:
+                print(link.get('href'))
+
+
+
+
+if __name__ == '__main__':
+    print("fetching data from: http://www.d20pfsrd.com/classes");
+    _parse(_fetch("http://www.d20pfsrd.com/classes"));
+
+
+
---
+++ main.js
@@ -0,0 +1,58 @@
+'use strict';
+var globalShortcut = require('global-shortcut');
+const electron = require('electron');
+const app = electron.app;  // Module to control application life.
+const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+var mainWindow = null;
+var dropWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    app.quit(); // quit for now becuse it is easier
+    // app.dock.hide();
+    // if (process.platform != 'darwin') {
+    //   app.quit();
+    // }
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+    globalShortcut.register('ctrl+shift+space', function () {
+        console.log('ctrl+shift+space');
+        if(dropWindow){
+            if(dropWindow.isVisible()){
+                dropWindow.hide();
+            }
+            else{
+                dropWindow.show();
+            }
+        }
+        else{
+            dropWindow = new BrowserWindow({width: 800, height: 600});
+            dropWindow.loadURL('file://' + __dirname + '/drop.html');
+        }
+
+    });
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+});
